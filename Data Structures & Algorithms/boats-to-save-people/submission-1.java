6d7f83a7d989a11c4b6c064fa5e1f8564b51ec30
Rakesh Kuratti <83536408+Rakesh-vk@users.noreply.github.com>
2026-07-09 14:17:36 +0530

    Add: boats-to-save-people - submission-1

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+
+        Arrays.sort(people);
+        int count=0;
+
+        int l = 0;
+        int r = people.length-1;
+
+        while(l<=r)
+        {
+
+            if(people[l]+people[r]>limit)
+            {
+                count++;
+                r--;
+
+            }else{
+                l++;
+                r--;
+                count++;
+            }
+        }
+
+        return count;
+        
+    }
+}
